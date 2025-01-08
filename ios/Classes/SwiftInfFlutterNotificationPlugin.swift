673e4467ae82c56efcb18a65f17a17f8eea5cf4e
baotran <brightsmile959674@gmail.com>
2025-01-08 23:13:59 +0700

    noti on foreground

--- ios/Classes/SwiftInfFlutterNotificationPlugin.swift
+++ ios/Classes/SwiftInfFlutterNotificationPlugin.swift
@@ -11,4 +11,14 @@ public class SwiftInfFlutterNotificationPlugin: NSObject, FlutterPlugin {
   public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
     result("iOS " + UIDevice.current.systemVersion)
   }
+
+  public func application(
+      _ application: UIApplication,
+      didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    return true
+  }
 }

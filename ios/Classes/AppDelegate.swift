aa0edbd7c2f32717d76a9b6314077ded58486fb9
baotran <brightsmile959674@gmail.com>
2025-01-08 23:26:08 +0700

    try add custom app delegate

---
+++ ios/Classes/AppDelegate.swift
@@ -0,0 +1,15 @@
+import UIKit
+import Flutter
+
+@UIApplicationMain
+@objc class AppDelegate: FlutterAppDelegate {
+  public func application(
+    _ application: UIApplication,
+    didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?
+  ) -> Bool {
+    if #available(iOS 10.0, *) {
+      UNUserNotificationCenter.current().delegate = self as? UNUserNotificationCenterDelegate
+    }
+    return true
+  }
+}
